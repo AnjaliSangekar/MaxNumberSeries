bc298de902e8c6a6fb6fa1b75ea53b63c1afebcc
Anjali Sangekar <99080406+AnjaliSangekar@users.noreply.github.com>
2024-06-20 06:52:01 -0500

    Add files via upload

---
+++ MaxNumberSeries/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumberSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a series of numbers, seperated by commas: ");
+
+            var Input = Console.ReadLine();
+
+            var numbers = Input.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                    max = number;
+            }
+
+            Console.WriteLine("Max number is " + max);
+
+        }
+    }
+}
